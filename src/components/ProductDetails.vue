83cd29847b3a59c0946241ea03d61c5798d41f08
darina9 <111124490+darina9@users.noreply.github.com>
2023-10-01 11:09:20 +0200

    Add files via upload

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <ProductItem
+        v-for="product in products" 
+        :key="product.id" 
+        :product="product"
+        />
+    </div>
+</template>
+
+<script>
+
+import ProductItem from './ProductItem.vue'
+export default {
+    name: 'ProductDetails',
+    components: {
+        ProductItem},
+   data() {      
+           
+           return {
+               products:[{
+                   id:1,
+                   name:'Barbi',
+                   price:2000,
+                   available:true
+               },
+               {   id:2,
+                   name:'Batman',
+                   price:2500,
+                   available:true
+               },
+               {   id:3,
+                   name:'Halk',
+                   price:1500,
+                   available:false
+               },
+           ],
+            
+           };
+       },         
+   
+  
+}
+
+</script>
+
+
+
+<style lang="scss" scoped>
+
+
+</style>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,45 @@
+<template>
+     <div>
+        <div class="product">
+        <h2> Название товара: <span class="product__name">{{ product.name }}</span></h2>
+        <h3>Цена в рублях: {{ product.price }} </h3>
+        <h3>Цена в долларах: {{ formattedPrice }}$</h3>
+        <h3 v-if="product.available"><span class="product__available">Available</span></h3>
+        <h3 v-else><span class="product__out">Out of stock</span></h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductItem',
+    props: ['product'],
+    data(){
+    return {
+          }},    
+
+    computed:{
+        formattedPrice() {
+            return this.product.price/100
+    }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.product{
+    margin: 50px auto;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    &__name{
+        color: #8A2BE2;
+    }
+    &__available{
+        color: chartreuse;
+    }
+    &__out{
+        color: brown;
+    }
+}
+</style>
